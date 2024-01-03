25a04300199319acaefafac025162c6bb1685759
KshitijV97 <writetokshitij@gmail.com>
2024-01-03 09:26:53 +0530

    Notes

---
+++ ColtSteele/ProblemSolvingPatterns/MultiplePointers.ts
@@ -0,0 +1,9 @@
+// Multiple pointers
+
+/**
+ * Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
+ */
+
+/** Very efficient for solving problems with minimum space complexity */
+
+
