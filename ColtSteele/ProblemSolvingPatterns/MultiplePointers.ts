55877de5b82dc8fbb36835bf9974ad95bc7ac044
KshitijV97 <writetokshitij@gmail.com>
2024-01-08 20:37:31 +0530

    SumZero using two pointers

--- ColtSteele/ProblemSolvingPatterns/MultiplePointers.ts
+++ ColtSteele/ProblemSolvingPatterns/MultiplePointers.ts
@@ -15,3 +15,18 @@ function sumZero(arr) {
 		}
 	}
 }
+
+function sumZeroTwoPointers(arr) {
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		let sum = arr[left] + arr[right];
+		if (sum === 0) {
+			return [arr[i], arr[j]];
+		} else if (sum > 0) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+}
