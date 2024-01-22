b88b5a5939ad9ff7630ce85d568af3d7f32d2315
KshitijV97 <writetokshitij@gmail.com>
2024-01-22 23:01:00 +0530

    Added example of sliding window

--- ColtSteele/ProblemSolvingPatterns/SlidingWindowPattern.ts
+++ ColtSteele/ProblemSolvingPatterns/SlidingWindowPattern.ts
@@ -24,3 +24,18 @@ function maxSubarraySum(arr, num) {
 	}
 	return max;
 }
+
+function maxSubarraySumSliding(arr, num) {
+	let maxSum = 0;
+	let tempSum = 0;
+	if (arr.length < num) return null;
+	for (let i = 0; i < num; i++) {
+		maxSum = maxSum + arr[i];
+	}
+	tempSum = maxSum;
+	for (let i = num; i < arr.length; i++) {
+		tempSum = tempSum + arr[i] - arr[i - num];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+	return maxSum;
+}
