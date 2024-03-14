0bda2135ccabff76b80ad9a266d371c2659bb31f
Kshitij Vengurlekar <writetokshitij@gmail.com>
2024-03-14 13:51:46 +0530

    Added notes

--- NamasteJs/S02_E04/01_async_await.js
+++ NamasteJs/S02_E04/01_async_await.js
@@ -1,7 +1,21 @@
 const p = new Promise(function (resolve, reject) {
-	resolve("Promise is resolved");
+  setTimeout(() => {
+    resolve("Promise is resolved");
+  }, 5000);
 });
 
 async function getData() {
-	return p;
+  p.then((res) => console.log(res));
+  console.log("Namaste kshitij");
 }
+
+// async and await combo are used to handle promises
+async function handlePromise() {
+  const val = await p; // Execution will get paused here
+  console.log(val);
+}
+
+handlePromise();
+
+// await can only be used inside async function
+getData();
