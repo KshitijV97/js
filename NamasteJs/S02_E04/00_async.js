045770f888da6bd10bf2552802808661139e0291
Kshitij Vengurlekar <writetokshitij@gmail.com>
2024-03-14 13:51:39 +0530

    Added notes

--- NamasteJs/S02_E04/00_async.js
+++ NamasteJs/S02_E04/00_async.js
@@ -3,10 +3,24 @@
 // Either we explicitly return a promise
 // If we return a value instead of a promise, The function will wrap the value inside a promise and then return it
 
+// Below function returns value
 async function getData() {
-	return "Namaste";
+  return "Namaste";
 }
 
 const dataPromise = getData();
 console.log(dataPromise); // This will print the promise
 dataPromise.then((res) => console.log(res)); // This will print the resolved value
+
+const p = new Promise((resolve, reject) => {
+  resolve("Promise resolved value");
+});
+
+// Below function returns promise
+async function getDataP() {
+  return p;
+}
+
+const dPromise = getDataP();
+console.log("dPromise", dPromise);
+dPromise.then((res) => console.log(res));
